59e7723c8d628e44437ce7a84c83c2709c6a8bba
Robert Malikov <Robert.Malikov@urfu.me>
2025-04-22 18:18:20 +0500

    1 fix

--- index.js
+++ index.js
@@ -7,17 +7,16 @@ addBtn.addEventListener('click', () => {
 
 const deleteBtn = document.querySelector('.delete-form-btn');
 deleteBtn.addEventListener('click', () => {
-    if (drinksCount === 1) {
-        return;
+    if (drinksCount > 1) {
+        drinksCount--;
+        deleteBtn.parentElement.parentElement.remove();
     }
-    drinksCount--;
-    deleteBtn.parentElement.parentElement.parentElement.remove();
 })
 
 function addNewForm() {
-    const newForm = document.createElement('form');
-    newForm.innerHTML = `      <fieldset class="beverage">
-        <div>
+    const newFieldSet = document.createElement('fieldset');
+    newFieldSet.classList.add('beverage');
+    newFieldSet.innerHTML = `        <div>
           <h4 class="beverage-count">Напиток №${++drinksCount}</h4>
           <label class="field">
             <span class="label-text">Я буду</span>
@@ -67,25 +66,14 @@ function addNewForm() {
           </div>
         </div>
         <div>
-          <button class="delete-form-btn">Удалить</button>
-        </div>
-      </fieldset>
-      <div>
-        <button type="button" class="add-button">+ Добавить напиток</button>
-      </div>
-      <div style="margin-top: 30px">
-        <button type="submit" class="submit-button">Готово</button>
-      </div>`
-    document.querySelector('form').after(newForm);
+          <button type="button" class="delete-form-btn">Удалить</button>
+        </div>`
+    document.querySelector('.beverage:last-of-type').after(newFieldSet);
     
-    const addBtns = document.querySelectorAll('.add-button');
-    addBtns[addBtns.length - 1].addEventListener('click', () => addNewForm());
-
-    const deleteBtns = document.querySelectorAll('.delete-form-btn');
-    const lastDeleteBtn = deleteBtns[deleteBtns.length - 1];
-    lastDeleteBtn.addEventListener('click', () => {
-        if (drinksCount >= 1) {
-            lastDeleteBtn.parentElement.parentElement.parentElement.remove();
+    const deleteBtn = newFieldSet.querySelector('.delete-form-btn:last-of-type');
+    deleteBtn.addEventListener('click', () => {
+        if (drinksCount > 1) {
+            deleteBtn.parentElement.parentElement.remove();
             drinksCount--;
         }
     })
